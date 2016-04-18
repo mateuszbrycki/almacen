94fd6ef24974531161bac58dba64a9c5de10eb29
Brolly <tomhanusiak@gmail.com>
2016-04-18 23:50:39 +0200

    [FILE-1] - fix split

--- src/main/java/com/almacen/module/file/FileController.java
+++ src/main/java/com/almacen/module/file/FileController.java
@@ -5,13 +5,14 @@ import com.almacen.module.file.service.FileService;
 import com.almacen.module.file.specification.UserFileSpecification;
 import com.almacen.module.user.exception.UserNotFoundException;
 import com.almacen.module.user.service.UserService;
-import com.almacen.specification.Specification;
 import com.almacen.util.UserUtils;
 import org.apache.log4j.Logger;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.FileCopyUtils;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -20,7 +21,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.util.Locale;
-import java.util.StringTokenizer;
 
 @Controller
 @RequestMapping(FileUrls.FILE)
@@ -51,15 +51,7 @@ public class FileController {
 
 
         if (!file.isEmpty()) {
-            StringTokenizer stringTokenizer = new StringTokenizer(file.getOriginalFilename(), ".");
-
-                    /*stringTokenizer.
-
-                System.out.println(fileArray[1]);
-                logger.debug(fileArray.toString());*/
-
-            String extension = "a";
-
+            String extension = file.getOriginalFilename().split("\\.")[1];
             userFile.setName(file.getOriginalFilename());
             userFile.setExtension(extension);
             userFile.setSize(file.getSize());
@@ -67,7 +59,6 @@ public class FileController {
         } else {
             return "redirect:" + BaseUrls.APPLICATION;
         }
-
         if (!specification.isSatisfiedBy(userFile)) {
             attributes.addFlashAttribute("error", messageSource.getMessage("file.extension.blocked", args, locale));
             return "redirect:bad_file";
